1d0fb1bdb07a013d34231206360c0f6886fa7843
jasam-sheja <jasam0jasam0jasam@gmail.com>
2021-12-06 14:20:59 +0900

    add documentation to `euclidean.cdist`

--- torch_dist/euclidean.py
+++ torch_dist/euclidean.py
@@ -64,7 +64,34 @@ class CdistFunction_optmem(Function):
         return grad_x1, grad_x2
 
 
-def cdist(x1: torch.Tensor, x2: torch.Tensor, opt: str = 'compute') -> torch.Tensor:
+def cdist(x1: torch.Tensor, x2: torch.Tensor, *, opt: str = 'compute') -> torch.Tensor:
+    r"""Computes batched the p-norm distance between each pair of the two collections of row vectors.
+
+    Args:
+        x1 (Tensor): input tensor of shape :math:`B \times P \times M`.
+        x2 (Tensor): input tensor of shape :math:`B \times R \times M`.
+        opt (str)  : focus optimization parameter. Either focus on `mem` for
+                     for memory or `compute` for flops
+
+    If x1 has shape :math:`B \times P \times M` and x2 has shape :math:`B \times R \times M` then the
+    output will have shape :math:`B \times P \times R`.
+
+    Example:
+
+        >>> a = torch.tensor([[0.9041,  0.0196], [-0.3108, -2.4423], [-0.4821,  1.059]])
+        >>> a
+        tensor([[ 0.9041,  0.0196],
+                [-0.3108, -2.4423],
+                [-0.4821,  1.0590]])
+        >>> b = torch.tensor([[-2.1763, -0.4713], [-0.6986,  1.3702]])
+        >>> b
+        tensor([[-2.1763, -0.4713],
+                [-0.6986,  1.3702]])
+        >>> torch_dist.euclidean.cdist(a, b)
+        tensor([[3.1193, 2.0959],
+                [2.7138, 3.8322],
+                [2.2830, 0.3791]])
+    """
     if opt == 'compute':
         return CdistFunction_optcompute.apply(x1, x2)
     elif opt == 'mem':
